4243d98311484c48c2bf4c4128d123f01e2bca62
Felix Bohnacker <Felix Bohnacker>
2016-07-13 08:19:47 +0200

    Initial commit

---
+++ src/javatutorium/Auto.java
@@ -0,0 +1,24 @@
+package javatutorium;
+
+
+public class Auto { // <= Klasse :)
+	
+	// 1. Attribute / Objekteigenschaften
+	private double motorleistung;
+	
+	// 2. Methoden
+	public Auto(int leistung){
+		this.motorleistung = leistung;
+	}
+	public void setMotorleistung(double zahl) {
+		if (zahl < 0){
+			System.out.println("Dieses Auto hat eine negative Leistung");
+		} else {
+			this.motorleistung = zahl;
+		}
+	}
+
+	public double getMotorleistung(){
+		return this.motorleistung;
+	}
+}
---
+++ src/javatutorium/Tut1.java
@@ -0,0 +1,20 @@
+package javatutorium;
+
+public class Tut1 {
+	public static void main(String[] args) {
+		// System.out.println(doubleInteger(46));
+
+		Auto polo = new Auto(123);
+		polo.setMotorleistung(111);
+		polo.setMotorleistung(150);
+		
+		Auto up = new Auto(20);
+		up.setMotorleistung(240);
+		System.out.println("Der VW Polo hat " + polo.getMotorleistung()+"Kw!");
+		System.out.println("Der VW Up   hat " + up.getMotorleistung()+"Kw!");
+	}
+
+	public static int doubleInteger(int zahl) {
+		return zahl * 2;
+	}
+}
